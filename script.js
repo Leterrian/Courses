c079dfa166626c113ba0dac823eb6fd694de4636
Leterrian <leterrian@icloud.com>
2025-02-16 22:14:36 -0500

    testing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const courseList = document.getElementById("course-list");
+const avaliabileCourses = document.getElementById("courses-conatainer");
 const semester1 = document.getElementById("semester1");
 const semester2 = document.getElementById("semester2");
 const semester3 = document.getElementById("semester3");
@@ -10,4 +10,22 @@ const courses = [
     {name: "System Analysis/Software Engineering" , preReqs: "Data Structures, Java", semester: "Fall/Spring", credits: 3, description: "An introduction to modern systems analysis and software engineering practices. Major topics include software development methodologies, the software development cycle, and software architectures."},
     {name: "Operating Systems" , preReqs: "Data Structures", semester: "Fall/Spring", credits: 3, description: "Basic concepts and terminology of operating systems, I/0 and interrupt structures, system structures, processor scheduling, processes and process synchronization, deadlocks, memory management, and other concepts."},
 ]
+
+
+
+
+
+// Display courses 
+function showDetails(course){
+    const courseDetails = document.getElementById("courses-container");
+    courseDetails.innerHTML = `
+        <h3>${course.name}</h3>
+        <p>Prerequisites: ${course.preReqs}</p>
+        <p>Semester: ${course.semester}</p>
+        <p>Credits: ${course.credits}</p>
+        <p>Description: ${course.description}</p>
+    `;
+}
+showDetails(courses[3]);
+
 
